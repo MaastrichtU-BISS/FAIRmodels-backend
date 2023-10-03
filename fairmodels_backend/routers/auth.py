1eec18fe4a1e118a1e164037d86071c0eb1a8daa
eensander <50321538+eensander@users.noreply.github.com>
2023-10-03 16:06:22 +0200

    refactor file structure and inherited FastAPI auth example

---
+++ fairmodels_backend/routers/auth.py
@@ -0,0 +1,88 @@
+from typing import Annotated, Union
+from fastapi import Depends, APIRouter, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+
+router = APIRouter(prefix="/auth")
+
+fake_users_db = {
+}
+
+
+def fake_hash_password(password: str):
+    return "fakehashed-" + password
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+class User(BaseModel):
+    username: str
+    email: Union[str, None] = None
+    full_name: Union[str, None] = None
+    disabled: Union[bool, None] = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def fake_decode_token(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(fake_users_db, token)
+    return user
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)]
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+class LoginBody(BaseModel):
+    username: str
+    password: str
+
+@router.post("/token")
+async def login(login_form: LoginBody):
+    user_dict = fake_users_db.get(login_form.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(login_form.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@router.get("/me")
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return current_user 
+
+# Legacy:
+# @app.get("/auth_link")
+# def cli_auth():
+#   api = orcid.PublicAPI(os.getenv("ORCID_CLIENT_ID"), os.getenv("ORCID_CLIENT_SECRET"), sandbox=False)
+#   url = api.get_login_url(scope="/authenticate", redirect_uri="http://0.0.0.0:3099/redirect")
+#   return {"url": url}
--- fairmodels_backend/routers/model.py
+++ fairmodels_backend/routers/model.py
@@ -1,15 +1,10 @@
 from typing import Optional
-from fastapi import FastAPI, Body
+from fastapi import APIRouter, Body
 from pydantic import BaseModel
-import uvicorn
-import orcid
 from dotenv import load_dotenv
-import os
-from fairmodels_backend.data_manager import DataManager
-# from fairmodels_backend.models import CreateModelModel, UpdateModelModel
+from data_manager import DataManager
 
-load_dotenv()
-app = FastAPI()
+router = APIRouter()
 
 class CreateModelModel(BaseModel):
   name: str
@@ -22,12 +17,8 @@ class UpdateModelModel(BaseModel):
   metadata_id: Optional[str]
   update_type: str
   update_description: Optional[str] = None
-
-@app.get("/")
-def root():
-  return {"message": "Hello Client"}
-
-@app.get("/model")
+  
+@router.get("/model")
 def list_models():
   ModelDataManager = DataManager("model")
   models = ModelDataManager.list_entities();
@@ -38,7 +29,7 @@ def list_models():
   
   return model_dict
 
-@app.post("/model")
+@router.post("/model")
 def create_model(data: CreateModelModel):
   ModelDataManager = DataManager("model")
 
@@ -46,34 +37,23 @@ def create_model(data: CreateModelModel):
 
   return {"message": "Model succesfully created", "id": id}
 
-@app.get("/model/{model_id}")
+@router.get("/model/{model_id}")
 def read_model(model_id):
   ModelDataManager = DataManager("model")
   data = ModelDataManager.read_entity(model_id)
 
   return data
 
-@app.patch("/model/{model_id}")
+@router.patch("/model/{model_id}")
 def update_model(model_id, data: UpdateModelModel):
   ModelDataManager = DataManager("model")
   data = ModelDataManager.update_entity(model_id, data.onnx_model, data.metadata_id, data.update_type, data.update_description)
 
   return data
 
-@app.delete("/model/{model_id}")
+@router.delete("/model/{model_id}")
 def delete_model(model_id):
   ModelDataManager = DataManager("model")
   ModelDataManager.delete_entity(model_id)
 
   return {"message": "Model succesfully deleted"}
-
-@app.get("/auth_link")
-def cli_auth():
-  api = orcid.PublicAPI(os.getenv("ORCID_CLIENT_ID"), os.getenv("ORCID_CLIENT_SECRET"), sandbox=False)
-  url = api.get_login_url(scope="/authenticate", redirect_uri="http://0.0.0.0:3099/redirect")
-
-  return {"url": url}
-
-if __name__ == "__main__":
-  import uvicorn
-  uvicorn.run(app, host="0.0.0.0", port=3099)
