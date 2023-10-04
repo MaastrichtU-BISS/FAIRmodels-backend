36f7021f0e778ea524ba19651f3f206eac867a66
eensander <50321538+eensander@users.noreply.github.com>
2023-10-04 14:50:50 +0200

    feat: update rename to datalayer

--- fairmodels_backend/routers/model.py
+++ fairmodels_backend/routers/model.py
@@ -2,7 +2,7 @@ from typing import Optional
 from fastapi import APIRouter, Body
 from pydantic import BaseModel
 from dotenv import load_dotenv
-from data_manager import DataManager
+from data_manager import ModelDataLayer
 
 router = APIRouter()
 
@@ -20,40 +20,40 @@ class UpdateModelModel(BaseModel):
   
 @router.get("/model")
 def list_models():
-  ModelDataManager = DataManager("model")
-  models = ModelDataManager.list_entities();
+  ModelData = ModelDataLayer()
+  models = ModelData.list()
   model_dict = {}
 
   for model_id in models:
-    model_dict[model_id] = ModelDataManager.read_entity(model_id)
+    model_dict[model_id] = ModelData.read(model_id)
   
   return model_dict
 
 @router.post("/model")
 def create_model(data: CreateModelModel):
-  ModelDataManager = DataManager("model")
+  ModelData = ModelDataLayer
 
-  id = ModelDataManager.create_entity(data.name, data.description, data.onnx_model, data.metadata_id)
+  id = ModelData.create(data.name, data.description, data.onnx_model, data.metadata_id)
 
   return {"message": "Model succesfully created", "id": id}
 
 @router.get("/model/{model_id}")
 def read_model(model_id):
-  ModelDataManager = DataManager("model")
-  data = ModelDataManager.read_entity(model_id)
+  ModelData = ModelDataLayer()
+  data = ModelData.read(model_id)
 
   return data
 
 @router.patch("/model/{model_id}")
 def update_model(model_id, data: UpdateModelModel):
-  ModelDataManager = DataManager("model")
-  data = ModelDataManager.update_entity(model_id, data.onnx_model, data.metadata_id, data.update_type, data.update_description)
+  ModelData = ModelDataLayer()
+  data = ModelData.update(model_id, data.onnx_model, data.metadata_id, data.update_type, data.update_description)
 
   return data
 
 @router.delete("/model/{model_id}")
 def delete_model(model_id):
-  ModelDataManager = DataManager("model")
-  ModelDataManager.delete_entity(model_id)
+  ModelData = ModelDataLayer()
+  ModelData.delete(model_id)
 
   return {"message": "Model succesfully deleted"}
