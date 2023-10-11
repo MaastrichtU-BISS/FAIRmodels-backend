2e1b39260c2243d63601e0e9f965896d5be0609d
eensander <50321538+eensander@users.noreply.github.com>
2023-10-11 16:03:36 +0200

    implement password hashing

--- fairmodels_backend/routers/auth.py
+++ fairmodels_backend/routers/auth.py
@@ -2,36 +2,23 @@ from typing import Annotated, Union
 from fastapi import Depends, APIRouter, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
-
-from fairmodels_backend.data_manager import UserDataLayer
+from data_layer import UserDataLayer
+import bcrypt
 
 router = APIRouter(prefix="/auth")
 
-def hash_password(password: str):
-    return "fakehashed-" + password
+def password_hash(password: str):
+    # print("hashing ", bytes.decode(bcrypt.hashpw("haa".encode('utf-8'), bcrypt.gensalt())) )
+    # return str(bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()))
+    return bytes.decode(bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()))
 
-
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": hash_password('secret'),
-        "disabled": False,
-    },
-    "alice": {
-        "username": "alice",
-        "full_name": "Alice Wonderson",
-        "email": "alice@example.com",
-        "hashed_password": hash_password('secret2'),
-        "disabled": True,
-    },
-}
-
+def password_check(password: str, hash: str):
+    return bcrypt.checkpw(password.encode('utf-8'), hash)
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 class User(BaseModel):
+    id: str
     username: str
     email: Union[str, None] = None
     full_name: Union[str, None] = None
@@ -41,18 +28,17 @@ class UserInDB(User):
     hashed_password: str
 
 
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
+def get_user(username: str):
+    user_layer = UserDataLayer()
+    user = user_layer.find_by_username()
+    return UserInDB(**user)
 
 
 def fake_decode_token(token):
     # This doesn't provide any security at all
     # Check the next version
-    user = get_user(fake_users_db, token)
+    user = get_user(token)
     return user
-
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     user = fake_decode_token(token)
@@ -63,14 +49,6 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
             headers={"WWW-Authenticate": "Bearer"},
         )
     return user
-
-
-async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_current_user)]
-):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
 
 class RegisterBody(BaseModel):
     username: str
@@ -84,25 +62,32 @@ class LoginBody(BaseModel):
 async def register(req: RegisterBody):
     UserData = UserDataLayer()
 
-    UserData.create(req.username, hash_password(req.password))
+    UserData.create(req.username, password_hash(req.password))
 
     return {"success": True}
 
 @router.post("/token")
 async def login(login_form: LoginBody):
-    user_dict = fake_users_db.get(login_form.username)
+    user_layer = UserDataLayer()
+    user_dict = user_layer.find_by_username(login_form.username)
     if not user_dict:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
     user = UserInDB(**user_dict)
-    hashed_password = hash_password(login_form.password)
-    if not hashed_password == user.hashed_password:
+    if not password_check(login_form.password, user.hashed_password):
         raise HTTPException(status_code=400, detail="Incorrect username or password")
 
     return {"access_token": user.username, "token_type": "bearer"}
 
+@router.post("/generate_api_key")
+async def generate_api_key(
+    user: Annotated[User, Depends(get_current_user)]
+):
+    user_layer = UserDataLayer()
+    user_layer.generate_api_key(user.id)
+
 @router.get("/me")
 async def read_users_me(
-    current_user: Annotated[User, Depends(get_current_active_user)]
+    current_user: Annotated[User, Depends(get_current_user)]
 ):
     return current_user
 
