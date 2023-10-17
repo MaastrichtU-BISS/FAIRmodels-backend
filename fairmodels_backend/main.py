0d7aeff02ed0a0a0bfeaa8869627131679711a36
eensander <50321538+eensander@users.noreply.github.com>
2023-10-17 16:41:10 +0200

    feat: basic containerization using dockerfile and docker-compose works

--- fairmodels_backend/main.py
+++ fairmodels_backend/main.py
@@ -2,4 +2,4 @@ import uvicorn
 
 if __name__ == "__main__":
   import uvicorn
-  uvicorn.run("server:app", host="0.0.0.0", port=3099, reload=True)
+  uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
