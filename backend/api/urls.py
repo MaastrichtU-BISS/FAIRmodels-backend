7f5d375ab0cff9d20f20bc0b1ff2bb9507a0199b
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2023-12-22 12:15:41 +0100

    implemented REST api for models

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,23 @@
-from django.urls import path
+from django.urls import path, include
+from .models import Fairmodel
+from rest_framework import routers, serializers, viewsets
 
-from . import views
+# Serializers define the API representation.
+class FairmodelSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Fairmodel
+        fields = ['id', 'name', 'description']
+
+# ViewSets define the view behavior.
+class FairmodelViewSet(viewsets.ModelViewSet):
+    queryset = Fairmodel.objects.all()
+    serializer_class = FairmodelSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'model', FairmodelViewSet)
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', include(router.urls)),
+    path('auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
