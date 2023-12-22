f7249831655ce74bb44b7ad002fda0f22d61ee8f
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2023-12-22 11:40:15 +0100

    Created first model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Fairmodel(models.Model):
+    id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=265)
+    description = models.TextField()
+
+class Fairmodel_Version(models.Model):
+    version = models.CharField(max_length=200)
+    onnx_model = models.TextField()
+    fairmodel = models.ForeignKey(Fairmodel, on_delete=models.CASCADE)
+    update_description = models.TextField()
+    created_at = models.DateTimeField()
