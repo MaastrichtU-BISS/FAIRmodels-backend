7f1d2382bf22f2c8481d0953779b6d32b0034c99
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2023-12-22 11:49:06 +0100

    Register objects in admin interface

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Fairmodel, Fairmodel_Version
+
+admin.site.register(Fairmodel)
+admin.site.register(Fairmodel_Version)
