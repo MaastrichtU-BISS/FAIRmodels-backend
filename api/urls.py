b87294af0129c1ce62610114443cb7feb2f7c740
eensander <50321538+eensander@users.noreply.github.com>
2024-02-14 17:27:14 +0100

    restructure urls and add link

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
-from .views import index, fairmodel_view, fairmodels_view, modelversions_view, modelversion_view, model_view
+from .views import \
+    index, \
+    cedar_instances, \
+    fairmodel_view, \
+    fairmodels_view, \
+    modelversions_view, \
+    modelversion_view, \
+    link_view, \
+    model_view
 
 urlpatterns = [
     path('', index),
+    path('cedar_instances/', cedar_instances),
     path('fairmodel/', fairmodels_view),
     path('fairmodel/<uuid:model_id>', fairmodel_view),
     path('fairmodel/<uuid:model_id>/version', modelversions_view),
     path('fairmodel/<uuid:model_id>/version/<uuid:version_id>', modelversion_view),
+    path('fairmodel/<uuid:model_id>/version/<uuid:version_id>/link', link_view),
     path('fairmodel/<uuid:model_id>/version/<uuid:version_id>/model', model_view),
 ]
