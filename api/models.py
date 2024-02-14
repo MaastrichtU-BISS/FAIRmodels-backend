3014e8b29166fed496376add57c0be8d6f3dede0
eensander <50321538+eensander@users.noreply.github.com>
2024-02-14 17:26:37 +0100

    add metadata_json and FeatureLink base model

--- api/models.py
+++ api/models.py
@@ -21,7 +21,8 @@ class FairmodelVersion(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     fairmodel = models.ForeignKey(Fairmodel, on_delete=models.CASCADE)
     version = models.CharField(max_length=255)
-    metadata_id = models.CharField(max_length=255)
+    metadata_id = models.CharField(max_length=255, null=True)
+    metadata_json = models.JSONField(null=True)
     update_description = models.TextField()
     model_type = models.CharField(choices=ModelType.choices, default=ModelType.DEFAULT, max_length=64)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -30,3 +31,18 @@ class FairmodelVersion(models.Model):
     def has_model(self):
         output_path = Path('storage/' + str(self.fairmodel.id) + '/' + str(self.id))
         return isfile(output_path)
+
+class FeatureLink(models.Model):
+    
+    class LinkType(models.TextChoices):
+        INPUT = 'INPUT'
+        OUTPUT = 'OUTPUT'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    fairmodel_version = models.ForeignKey(FairmodelVersion, on_delete=models.CASCADE)
+    link_type = models.CharField(choices=LinkType.choices, default=LinkType.INPUT, max_length=64)
+
+    # reference to field in metadata (to index in list Input/Outcome)
+    field_metadata = models.IntegerField()
+    # reference to field in pmml/onnx representation
+    field_model = models.CharField(max_length=255)
