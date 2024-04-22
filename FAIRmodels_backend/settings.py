08dd5b8563ed562513dc9ce3495ea292108b41ed
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2024-04-22 13:51:42 +0200

    removed credentials

--- FAIRmodels_backend/settings.py
+++ FAIRmodels_backend/settings.py
@@ -114,12 +114,12 @@ if os.getenv("DB_ENGINE", 'django.db.backends.sqlite3') == 'django.db.backends.s
 else:
     DATABASES = {
         'default': {
-            'ENGINE': os.getenv("DB_ENGINE", 'django.db.backends.postgresql'),
-            'NAME': os.getenv("DB_NAME", 'models'),
-            'USER': os.getenv("DB_USER", 'postgres'), 
-            'PASSWORD': os.getenv("DB_PASSWORD", 'postgres'),
-            'HOST': os.getenv("DB_HOST", 'db'),
-            'PORT': os.getenv("DB_PORT", '5432')
+            'ENGINE': os.getenv("DB_ENGINE"),
+            'NAME': os.getenv("DB_NAME"),
+            'USER': os.getenv("DB_USER"), 
+            'PASSWORD': os.getenv("DB_PASSWORD"),
+            'HOST': os.getenv("DB_HOST"),
+            'PORT': os.getenv("DB_PORT")
         }
     }
 
