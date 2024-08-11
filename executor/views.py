4bb01742e34a1ba53e91dafd6234c1d87b8fcc4f
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2024-08-11 23:14:12 +0200

    Execution of the given ONNX file

--- executor/views.py
+++ executor/views.py
@@ -40,20 +40,36 @@ def executor(request, model_id):
     logging.debug(model_version.metadata_input_variables)
     
     if request.method == 'GET':
+        # Show the view to enter the input values
         return render(request, 'executor.html', context={'model_version': model_version, 'title': parser.get_value(predicate="http://purl.org/dc/terms/title")})
     elif request.method == 'POST':
+        # Execute the model
+
+        # retrieve the entered values
         entered_values = request.POST.dict()
         logging.debug("Entered values: ")
         logging.debug(entered_values)
 
+        # Match the entered values with the model input variables and add them in the correct order
         variable_links = VariableLink.objects.filter(fairmodel_version=model_version).order_by("field_model_var_dim_start").all()
-        input_numbers = [ ]
+        input_numbers = { }
+        logging.debug("================Loop over variable links================")
         for variable_link in variable_links:
+            if variable_link.variable_type != VariableLink.VariableType.INPUT:
+                continue
+            logging.debug(variable_link)
             logging.debug(str(variable_link.field_metadata_var_id) + " | " + str(variable_link.field_model_var_name) + " | " + str(variable_link.field_model_var_dim_index) + " | " + str(variable_link.field_model_var_dim_start) + " | " + str(variable_link.field_model_var_dim_end) + " | " + entered_values[variable_link.field_metadata_var_id])
-            input_numbers.append(float(entered_values[variable_link.field_metadata_var_id]))
+            if variable_link.field_model_var_name not in input_numbers:
+                input_numbers[variable_link.field_model_var_name] = [[ ]]
+            input_numbers[variable_link.field_model_var_name][0].append(float(entered_values[variable_link.field_metadata_var_id]))
         logging.debug(input_numbers)
 
+        # Fetch the ONNX object from storage and execute it
         model_path = Path('storage/' + str(model_version.fairmodel.id) + '/' + str(model_version.id))
         onnx_session = ort.InferenceSession(str(model_path))
-        pass
-    return render(request, 'executor.html', context={'model_version': model_version, 'title': parser.get_value(predicate="http://purl.org/dc/terms/title")})
+        onnx_output = onnx_session.run(None, input_numbers)
+        logging.debug(onnx_output)
+        onnx_output = onnx_output[1][0]
+    
+    # Show the results
+    return render(request, 'executor.html', context={'model_version': model_version, 'title': parser.get_value(predicate="http://purl.org/dc/terms/title"), 'onnx_output': onnx_output})
